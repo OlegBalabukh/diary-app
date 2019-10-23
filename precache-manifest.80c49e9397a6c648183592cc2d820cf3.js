2cab5e5e6433cb43a3ea60d4aa06c17af1c33ef1
Oleg Balabukh <olegukoff@gmail.com>
2019-10-23 14:47:34 +0300

    Updates

---
+++ precache-manifest.80c49e9397a6c648183592cc2d820cf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aaa1ff7b39f18ba173dc9a11073f513",
+    "url": "/diary-app/index.html"
+  },
+  {
+    "revision": "49add8a159c490a2b2c8",
+    "url": "/diary-app/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "4288c3889d6fc0d8cb4c",
+    "url": "/diary-app/static/css/main.8988c7e5.chunk.css"
+  },
+  {
+    "revision": "49add8a159c490a2b2c8",
+    "url": "/diary-app/static/js/2.87dcb231.chunk.js"
+  },
+  {
+    "revision": "4288c3889d6fc0d8cb4c",
+    "url": "/diary-app/static/js/main.6e24ae74.chunk.js"
+  },
+  {
+    "revision": "3873db4c78e769eabd93",
+    "url": "/diary-app/static/js/runtime-main.840740c6.js"
+  }
+]);
